796927c8b11af426e1d557cb3f81bb59d3f45f39
S H O A I B <shoaibkarim159@gmail.com>
2021-08-11 01:19:29 +0600

    UI ADDED

--- login.php
+++ login.php
@@ -5,18 +5,184 @@
     <title>Login Page</title>
   </head>
   <style>
-    img {
-      width:100px;
-      height:75px;
-    }
-  </style>
-  <body>
-    <a href="login.php"> <img src="quiz.jpg" alt="home page link" /></a>
-    <h1>Welcome</h1>
-      <form method="get" action="selection.php">
-        <div >Enter your name!</div><br>
-        <input type="text" name="input_name"><br>
-        <input type="submit" value='Start quiz!'>
-      </form>
-  </body>
+*{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+main{
+  height: 100vh;
+  background: url('https://images.unsplash.com/photo-1477346611705-65d1883cee1e?dpr=0.800000011920929&auto=format&fit=crop&w=1199&h=800&q=80&cs=tinysrgb&crop=') fixed no-repeat;
+  background-size: cover;
+}
+#container{
+  width: 350px;
+  height: 500px;
+  background: inherit;
+  position: absolute;
+  overflow: hidden;
+  top: 50%;
+  left: 50%;
+  margin-left: -175px;
+  margin-top: -250px;
+  border-radius: 8px;
+}
+#container:before{
+  width: 400px;
+  height: 550px;
+  content: "";
+  position: absolute;
+  top: -25px;
+  left: -25px;
+  bottom: 0;
+  right: 0;
+  background: inherit;
+  box-shadow: inset 0 0 0 200px rgba(255,255,255,0.2);
+  filter: blur(10px);
+}
+form img{
+  width: 120px;
+  height: 120px;
+  border-radius: 100%;
+}
+form{
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+input{
+  background: 0;
+  width: 200px;
+  outline: 0;
+  border: 0;
+  border-bottom: 2px solid rgba(255,255,255, 0.3);
+  margin: 20px 0;
+  padding-bottom: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: rgba(255,255,255, 0.8);
+  text-align: center; 
+}
+input[type="submit"]{
+  border: 0;
+  border-radius: 8px;
+  padding-bottom: 0;
+  height: 60px;
+  background: #df2359;
+  color: white;
+  cursor: pointer;
+  transition: all 600ms ease-in-out;
+}
+input[type="submit"]:hover{
+  background: #C0392B;
+}
+span a{
+  color: rgba(255,255,255, 0.8);
+}
+</style>
+     <body>
+       <main>
+          <div id="container">
+             <a href="login.php"> </a> -->
+            <form method="get" action="selection.php">
+            
+              <img src="quiz.jpg" alt="home page link" /><br>
+              <div class="t1" >ENTER YOUR NAME</div><br>
+              <input type="text" name="input_name" autocomplete="off"><br>
+              <input type="submit" value='Start quiz!'>
+            </form>
+          </div>
+       </main>
+    </body>
 </html>
+
+<!-- <style>
+*{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+main{
+  height: 100vh;
+  background: url('https://images.unsplash.com/photo-1477346611705-65d1883cee1e?dpr=0.800000011920929&auto=format&fit=crop&w=1199&h=800&q=80&cs=tinysrgb&crop=') fixed no-repeat;
+  background-size: cover;
+}
+#container{
+  width: 350px;
+  height: 500px;
+  background: inherit;
+  position: absolute;
+  overflow: hidden;
+  top: 50%;
+  left: 50%;
+  margin-left: -175px;
+  margin-top: -250px;
+  border-radius: 8px;
+}
+#container:before{
+  width: 400px;
+  height: 550px;
+  content: "";
+  position: absolute;
+  top: -25px;
+  left: -25px;
+  bottom: 0;
+  right: 0;
+  background: inherit;
+  box-shadow: inset 0 0 0 200px rgba(255,255,255,0.2);
+  filter: blur(10px);
+}
+form img{
+  width: 120px;
+  height: 120px;
+  border-radius: 100%;
+}
+form{
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+input{
+  background: 0;
+  width: 200px;
+  outline: 0;
+  border: 0;
+  border-bottom: 2px solid rgba(255,255,255, 0.3);
+  margin: 20px 0;
+  padding-bottom: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: rgba(255,255,255, 0.8);
+}
+input[type="submit"]{
+  border: 0;
+  border-radius: 8px;
+  padding-bottom: 0;
+  height: 60px;
+  background: #df2359;
+  color: white;
+  cursor: pointer;
+  transition: all 600ms ease-in-out;
+}
+input[type="submit"]:hover{
+  background: #C0392B;
+}
+span a{
+  color: rgba(255,255,255, 0.8);
+}
+</style> -->
+
+<!-- <main>
+<div id="container">
+  <form action="">
+    <img src="quiz.jpg" alt="home page link" /><br>
+    <input type="text" name="input_name" autocomplete="off"><br>
+    
+    <input type="submit" value="SIGN IN"><br>
+  </form>
+</div>
+</main> -->
--- selection.php
+++ selection.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
   if(isset($_GET) && !empty($_GET)){
-      echo "<br>";
-      print_r($_GET["input_name"]);
+      // echo "<br>";
+      // print_r($_GET["input_name"]);
           $_SESSION["user_name"] = $_GET["input_name"];
           // echo "session added"; 
   }
@@ -18,6 +18,116 @@ session_start();
     width:100px;
     height:75px;
   }
+  *{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+main{
+  height: 100vh;
+  background: url('https://images.unsplash.com/photo-1477346611705-65d1883cee1e?dpr=0.800000011920929&auto=format&fit=crop&w=1199&h=800&q=80&cs=tinysrgb&crop=') fixed no-repeat;
+  background-size: cover;
+}
+#container{
+  width: 350px;
+  height: 500px;
+  background: inherit;
+  position: absolute;
+  overflow: hidden;
+  top: 50%;
+  left: 50%;
+  margin-left: -175px;
+  margin-top: -250px;
+  border-radius: 8px;
+}
+#container:before{
+  width: 400px;
+  height: 550px;
+  content: "";
+  position: absolute;
+  top: -25px;
+  left: -25px;
+  bottom: 0;
+  right: 0;
+  background: inherit;
+  box-shadow: inset 0 0 0 200px rgba(255,255,255,0.2);
+  filter: blur(10px);
+}
+form img{
+  width: 120px;
+  height: 120px;
+  border-radius: 100%;
+}
+form {
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+form {
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+input [type="submit"] [class="1"]{
+  background: 0;
+  width: 200px;
+  outline: 0;
+  border: 0;
+  margin: 20px 0;
+  padding-bottom: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: rgba(255,255,255, 0.8);
+}
+input[type="submit"]{
+  border: 0;
+  border-radius: 8px;
+  padding-bottom: 0;
+  height: 60px;
+  background: #df2359;
+  color: white;
+  cursor: pointer;
+  transition: all 600ms ease-in-out;
+}
+input[type="submit"]{
+  background: 0;
+  width: 200px;
+  outline: 0;
+  border: 0;
+  margin: 20px 0;
+  padding-bottom: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: rgba(255,255,255, 0.8);
+}
+input[type="submit"]{
+  border: 0;
+  border-radius: 8px;
+  padding-bottom: 0;
+  height: 60px;
+  background: #df2359;
+  color: white;
+  cursor: pointer;
+  transition: all 600ms ease-in-out;
+}
+
+
+
+input[type="submit"]:hover{
+  background: #C0392B;
+}
+span a{
+  color: rgba(255,255,255, 0.8);
+}
+.lit {
+    margin-top: 250px;
+}
+
+
   </style>
   <body>
     
@@ -32,17 +142,24 @@ session_start();
        
     ?>
     <h1> Select your quiz </h1>
+    <main>
 
-    <form action="literature.php">
-  		<div class="a">Literature!</div>
-  		<br>
-  		<input type="submit" value='Click here to try a quiz on Movies!'>
+    <div id="container">
+    <form action="literature.php" class="lit1">
+      <div class="lit">
+  		<input type="submit" class="1" value='Literature'>
+    </div>
   	</form>
   		<br><br>
-  	<form action="maths.php">
-  		<div class="a">Maths!</div>
-  		<br>
-  		<input type="submit" value='Click here to try a quiz on Sports!'>
+  	<form action="maths.php" class="mat1">
+      <div class="mat">
+  		<input type="submit" class="2" value='Mathematics'>
+    </div>
   	</form>
+    </div>
+    </main>
+
   </body>
 </html>
+
+
